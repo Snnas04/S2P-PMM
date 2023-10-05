b160c8eac00ddb23dad3612b25c4ea913546c8fd
Marc <marcsans26@gmail.com>
2023-10-05 16:31:40 +0200

    exercisi 1 practica tema 1, comentat, format hoes

--- Tema1/Practica1/Exercisi1.dart
+++ Tema1/Practica1/Exercisi1.dart
@@ -1,8 +1,8 @@
-// Realitza un programa, que mitjançant una funció la qual rep per paràmetres tres valors corresponents a hores, minuts i segons en format int, imprimeixi la hora en format: 01:49:11
+// // Realitza un programa, que mitjançant una funció la qual rep per paràmetres tres valors corresponents a hores, minuts i segons en format int, imprimeixi la hora en format: 01:49:11
 
 void main() {
-  int hora = 240;
-  int min = 434;
+  int hora = 220;
+  int min = 34;
   int seg = 32;
 
   formatHores(hora, min, seg);
@@ -10,6 +10,7 @@ void main() {
 
 void formatHores(int hora, int min, int seg) {
   int diesPassats = 0;
+  String horaFormatada;
 
   // Amb els tres bucles controlam que feim que si el valor introduit supera un valor valid
   // En el cas de sobrepassar aquest valor valid feim la diferencia i sumam un al parametre major, en el cas de lse hores anam sumant dies
@@ -28,5 +29,12 @@ void formatHores(int hora, int min, int seg) {
     diesPassats++;
   }
 
-  print("$diesPassats dies - $hora:$min:$seg");
+  if (diesPassats == 0) {
+    horaFormatada = "${hora.toString().padLeft(2, '0')}:${min.toString().padLeft(2, '0')}:${seg.toString().padLeft(2, '0')}";
+  } 
+  else {
+    horaFormatada = "${diesPassats.toString().padLeft(2, '0')} dies - ${hora.toString().padLeft(2, '0')}:${min.toString().padLeft(2, '0')}:${seg.toString().padLeft(2, '0')}";
+  }
+
+  print(horaFormatada);
 }
---
+++ Tema1/Practica1/Exercisi3.dart
@@ -0,0 +1,5 @@
+// Se’ns demana que programem el sistema per a gestionar un rent a car a la costa nord de Mallorca. Aquest rent a car, lloga vehicles de diferents tipus, cotxes i motos a client. Realitza les classes amb els seus atributs i mètodes/funcions per a assolir totes les funcionalitats desitjades.
+
+void main() {
+  
+}
