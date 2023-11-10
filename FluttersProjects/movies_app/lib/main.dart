46ba19a5774742ae0ed15e0f685d3d87af8cf5c9
Marc <marcsans26@gmail.com>
2023-11-10 16:14:03 +0100

    copiar projecte movies_app

---
+++ FluttersProjects/movies_app/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/providers/movies_provider.dart';
+import 'package:movies_app/screens/screens.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class AppState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: ( _ ) => MoviesProvider())
+      ]
+      );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pel·lícules',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => HomeScreen(),
+        'details': (BuildContext context) => DetailsScreen(),
+      },
+      theme: ThemeData.light()
+          .copyWith(appBarTheme: const AppBarTheme(color: Colors.indigo)),
+    );
+  }
+}
