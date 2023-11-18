9716bf85d41f619a13b4ba7ce655487a77db6aeb
Marc <marcsans26@gmail.com>
2023-11-18 11:57:24 +0100

    creacio del projecte de la practica

---
+++ FluttersProjects/practica_t2/lib/screens/home_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/providers/movies_provider.dart';
+import 'package:movies_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final moviesProvider = Provider.of<MoviesProvider>(context);
+
+    // print(moviesProvider.onDisplayMovies);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cartellera'),
+        elevation: 0,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              // Targetes principals
+              CardSwiper(movies: moviesProvider.onDisplayMovies),
+
+              // Slider de pel·licules
+              MovieSlider(),
+              // Poodeu fer la prova d'afegir-ne uns quants, veureu com cada llista és independent
+              // MovieSlider(),
+              // MovieSlider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
